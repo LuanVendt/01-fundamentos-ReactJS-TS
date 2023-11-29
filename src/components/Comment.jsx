25b3b1794f89aa7b7b61ca4a67d7c93c59bcd2bb
Luan Vendt <luan.vendt@bitize.com.br>
2023-11-29 15:46:27 -0300

    Initial commit

---
+++ src/components/Comment.jsx
@@ -0,0 +1,49 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+export function Comment({ content, onDeleteComment}) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+        
+        onDeleteComment(content)
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        })
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/111096073?v=4" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Luan Vendt</strong>
+                            <time title='24 de Novembro às 16:18h' dateTime='20220-05-11 16:17:30'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp/>
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import Styles from './Header.module.css';
+
+import igniteLogo from '../assets/ignite-logo.svg';
+
+export function Header() {
+    return (
+        <header className={Styles.header}>
+            <img src={igniteLogo} alt="Logotipo do Ignite" />
+            <h1>Ignite Feed</h1>
+        </header>
+    )
+}
